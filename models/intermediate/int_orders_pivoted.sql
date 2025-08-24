ac8f17e1db8711a8972b782adfce2673f6b8347a
yuri.novochadlo@outlook.com <yuri.novochadlo@outlook.com>
2025-08-24 16:25:08 +0000

    Criada a intermediate

---
+++ models/intermediate/int_orders_pivoted.sql
@@ -0,0 +1,19 @@
+with payment as 
+    (
+    select * from {{ ref('stg_stripe__payments') }}
+    where payment_status = 'success'
+)
+,pivoted as 
+(   
+    Select order_id
+        , sum(case when payment_method = 'bank_transfer' then payment_amount else 0 end) as bank_transfer_amount
+        , sum(case when payment_method = 'coupon' then payment_amount else 0 end) as coupon_amount
+        , sum(case when payment_method = 'credit_card' then payment_amount else 0 end) as credit_card_amount
+        , sum(case when payment_method = 'gift_card' then payment_amount else 0 end) as gift_card_amount
+    from payment
+    group by 1
+
+)
+
+select * 
+from pivoted
